5347447751d2c2c28ba1cc6b084f46df7b8b35e7
oncomouse <oncomouse@gmail.com>
2023-04-04 11:27:01 -0500

    feat: working lua implementation

--- lua/sort-motion/init.lua
+++ lua/sort-motion/init.lua
@@ -1,52 +1,52 @@
--- autoload/sort_motion.vim
-
-local sort_motion_flags = vim.g.sort_motion_flags
+local sort_motion_flags = vim.g.sort_motion_flags or ""
 
 local M = {}
 
 function M.sort_motion(mode)
 	local sort_motion_visual_block_command = vim.g.sort_motion_visual_block_command or "sort"
 
-  if mode == 'line' then
-	  vim.cmd(string.format([[execute "'[,']sort %"]], sort_motion_flags))
-  elseif mode == 'char' then
-	  local unnamed = table.concat(vim.fn.getreginfo([["]]).regcontents, "\n")
-    vim.cmd([[execute "normal! `[v`]y"]])
-    local startpos = vim.fn.match(unnamed, [[\v\i]])
-    local parts = vim.fn.split(unnamed, [[\v\i+]])
-	local prefix, delimiter, suffix
-    if startpos > 0 then
-      prefix = parts[0]
-      delimiter = parts[1]
-      suffix = parts[-1]
-    else
-      prefix = ''
-      delimiter = parts[0]
-      suffix = ''
-    end
-    if prefix == delimiter then
-      prefix = ''
-    end
-    if suffix == delimiter then
-      suffix = ''
-    end
-    local sortstart = #prefix
-    local sortend = #unnamed - sortstart - #suffix
-    local sortables = vim.fn.strpart(unnamed, sortstart, sortend)
-    local sorted = vim.fn.join(vim.fn.sort(vim.fn.split(sortables, [[\V]] .. vim.fn.escape(delimiter, [[\]]))), delimiter)
-    vim.cmd(string.format([[execute "normal! v`]c%s%s%"]] , prefix , sorted , suffix))
-    vim.cmd([[execute "normal! `["]])
-  elseif mode == 'V' then
-    vim.cmd(string.format([[execute "'<,'>sort %s"]],sort_motion_flags))
-  elseif mode == '' then
-    vim.cmd(string.format([[execute "'<,'>%s %s"]],sort_motion_visual_block_command, sort_motion_flags))
-  end
+	if mode == "line" then
+		vim.cmd("'[,']sort " .. sort_motion_flags)
+	elseif mode == "char" then
+		vim.cmd("normal! `[v`]y")
+		local unnamed = table.concat(vim.fn.getreginfo([["]]).regcontents, "\n")
+		local startpos = vim.fn.match(unnamed, [[\v\i]])
+		local parts = vim.fn.split(unnamed, [[\v\i+]])
+		local prefix, delimiter, suffix
+		if startpos > 0 then
+			prefix = parts[1]
+			delimiter = parts[2]
+			suffix = parts[#parts]
+		else
+			prefix = ""
+			delimiter = parts[1]
+			suffix = ""
+		end
+		if prefix == delimiter then
+			prefix = ""
+		end
+		if suffix == delimiter then
+			suffix = ""
+		end
+		local sortstart = #prefix
+		local sortend = #unnamed - sortstart - #suffix
+		local sortables = vim.fn.strpart(unnamed, sortstart, sortend)
+		local sorted =
+			vim.fn.join(vim.fn.sort(vim.fn.split(sortables, [[\V]] .. vim.fn.escape(delimiter, [[\]]))), delimiter)
+		vim.print(sorted)
+		vim.cmd(string.format("normal! v`]c%s%s%s", prefix, sorted, suffix))
+		vim.cmd("normal! `[")
+	elseif mode == "V" then
+		vim.cmd(string.format("'<,'>sort %s", sort_motion_flags))
+	elseif mode == "" then
+		vim.cmd(string.format("'<,'>%s %s", sort_motion_visual_block_command, sort_motion_flags))
+	end
 end
 
 function M.sort_lines()
-  local beginning = vim.fn.line('.')
-  local ed = vim.v.count1 + beginning - 1
-  vim.cmd([[execute %d,%dsort %s]], beginning, ed, sort_motion_flags)
+	local beginning = vim.fn.line(".")
+	local ed = vim.v.count1 + beginning - 1
+	vim.cmd(string.format("%d,%dsort %s", beginning, ed, sort_motion_flags))
 end
 
 return M
--- plugin/sort_motion.lua
+++ plugin/sort_motion.lua
@@ -3,10 +3,20 @@
 -- Version:      0.1
 -- Source:       http://github.com/oncomouse/sort-motion.nvim
 
-vim.keymap.set("n", "<Plug>(SortMotion)", "<cmd>set operatorfunc=sort_motion#sort_motion<CR>g@")
-vim.keymap.set("x", "<Plug>(SortMotionVisual)", "<cmd>call sort_motion#sort_motion('visual')<CR>")
-vim.keymap.set("n", "<Plug>(SortMotionLines)", "<cmd>call sort_motion#sort_lines()<CR>")
+vim.keymap.set("n", "<Plug>(SortMotion)", "<cmd>set operatorfunc=v:lua.require'sort-motion'.sort_motion<CR>g@")
+vim.keymap.set("x", "<Plug>(SortMotionVisual)", "<cmd>lua require('sort-motion').sort_motion(vim.fn.visualmode())<CR>")
+vim.keymap.set("n", "<Plug>(SortMotionLines)", "<cmd>lua require('sort-motion').sort_lines()<CR>")
 
-vim.keymap.set("n", "gs", "<Plug>(SortMotion)")
-vim.keymap.set("x", "gs", "<Plug>(SortMotionVisual)")
-vim.keymap.set("n", "gss", "<Plug>(SortMotionLines)")
+-- vim.keymap.set("n", "<Plug>(SortMotion)", "<cmd>set operatorfunc=sort_motion#sort_motion<CR>g@")
+-- vim.keymap.set("x", "<Plug>(SortMotionVisual)", "<cmd>call sort_motion#sort_motion(visualmode())<CR>")
+-- vim.keymap.set("n", "<Plug>(SortMotionLines)", "<cmd>call sort_motion#sort_lines()<CR>")
+
+if vim.fn.hasmapto("<Plug>(SortMotion)", "n") == 0 and vim.fn.maparg("gs", "n") == "" then
+	vim.keymap.set("n", "gs", "<Plug>(SortMotion)")
+end
+if vim.fn.hasmapto("<Plug>(SortMotionVisual)", "x") == 0 and vim.fn.maparg("gs", "x") == "" then
+	vim.keymap.set("x", "gs", "<Plug>(SortMotionVisual)")
+end
+if vim.fn.hasmapto("<Plug>(SortMotionLines)", "n") == 0 and vim.fn.maparg("gss", "n") == "" then
+	vim.keymap.set("n", "gss", "<Plug>(SortMotionLines)")
+end
